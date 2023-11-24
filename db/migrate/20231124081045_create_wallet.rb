cd707421240b7aa9243e340d3c6b049366fb6c74
Miko Dagatan <miguel.dagatan@gmail.com>
2023-11-24 17:30:47 +0800

    Add indexes to resources

--- db/migrate/20231124081045_create_wallet.rb
+++ db/migrate/20231124081045_create_wallet.rb
@@ -1,7 +1,7 @@
 class CreateWallet < ActiveRecord::Migration[7.0]
   def change
     create_table :wallets do |t|
-      t.references :entity, polymorphic: true, null: false
+      t.references :entity, polymorphic: true, null: false, index: true
       t.decimal :balance, precision: 19, scale: 2, default: 0.0
 
       t.timestamps
--- db/migrate/20231124081618_create_transactions.rb
+++ db/migrate/20231124081618_create_transactions.rb
@@ -1,11 +1,9 @@
 class CreateTransactions < ActiveRecord::Migration[7.0]
   def change
     create_table :transactions do |t|
-      t.references :source_wallet, foreign_key: { to_table: :wallets }
-      t.references :target_wallet, foreign_key: { to_table: :wallets }
+      t.references :source_wallet, foreign_key: { to_table: :wallets }, index: true
+      t.references :target_wallet, foreign_key: { to_table: :wallets }, index: true
       t.decimal :amount, precision: 19, scale: 2
-      # Add other necessary columns for your transactions
-      # For example, you might need a column for transaction type, timestamp, etc.
 
       t.timestamps
     end
