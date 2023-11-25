c5ab456f9980475ea5602d0cd23cc71560fb2871
Miko Dagatan <miguel.dagatan@gmail.com>
2023-11-25 09:17:58 +0800

    Git ignore coverage folder

--- spec/services/users/create_from_google_auth_spec.rb
+++ spec/services/users/create_from_google_auth_spec.rb
@@ -2,6 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Users::CreateFromGoogleAuth, type: :service do
   describe '#call' do
-    expect(described_class.new({}).call).to eq(1)
+    context 'when successful' do
+      it 'creates a user' do
+        expect do
+          described_class.new(google_auth_params).call
+        end.to change(User, :count).by(1)
+      end
+
+      it 'creates a wallet' do
+        expect do
+          described_class.new(google_auth_params).call
+        end.to change(Wallet, :count).by(1)
+      end
+    end
+
+    context 'when unsuccessful' do
+      let(:params) do
+        params = google_auth_params
+        params['names']['familyName'] = ''
+        params
+      end
+
+      it 'raises error' do
+        expect do
+          described_class.new(params).call
+        end.to raise_error(StandardError)
+      end
+    end
   end
 end
---
+++ spec/support/google_auth_mock.rb
@@ -0,0 +1,20 @@
+def google_auth_params
+  { 'resourceName' => 'people/106155691539393236437',
+    'etag' => '%EgcBAgkuNz0+GgQBAgUHIgxRWjZaUXg3N1NGWT0=',
+    'names' =>
+     [{ 'metadata' =>
+        { 'primary' => true, 'source' => { 'type' => 'PROFILE', 'id' => '106155691539393236437' },
+          'sourcePrimary' => true },
+        'displayName' => 'Miguel Dagatan',
+        'familyName' => 'Dagatan',
+        'givenName' => 'Miguel',
+        'displayNameLastFirst' => 'Dagatan, Miguel',
+        'unstructuredName' => 'Miguel Dagatan' }],
+    'emailAddresses' =>
+     [{ 'metadata' =>
+        { 'primary' => true,
+          'verified' => true,
+          'source' => { 'type' => 'ACCOUNT', 'id' => '106155691539393236437' },
+          'sourcePrimary' => true },
+        'value' => 'miguel.dagatan@gmail.com' }] }
+end
